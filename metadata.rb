5432337918e4c6b704d8be821f383046dca7a388
Michael Proctor-Smith <mproctor13@gmail.com>
2015-01-25 03:18:21 -0800

    bump version becase I forgot before.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "mproctor13@gmail.com"
 license          "Apache 2.0"
 description      "Use ohai plugin to set ipaddress attribute to the address of alternate device."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.1.1"
 
 recipe "alt_device_ipaddress::default", "Uses ohai plugin to set ipaddress attribute to address of alternate device."
 depends "ohai"
